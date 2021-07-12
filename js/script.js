53d548380c2b6930bf5aed5c0a2876a805a003d5
Vlad <vlad@gmail.com>
2021-07-12 13:37:53 +0300

    slider

---
+++ js/script.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+$('.feedback-container').slick({
+    dots: true,
+    infinite: true,
+    arrows: false,
+    speed: 300,
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: true
+            }
+        }
+    ]
+})
+});
