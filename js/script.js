77ab5fa5e427accc31fd52ab0cb3518e72ca6af7
Vlad <vlad@gmail.com>
2021-07-15 18:44:58 +0300

    SLIDER WORK

--- js/script.js
+++ js/script.js
@@ -1,21 +1,36 @@
-$(document).ready(function(){
-$('.slider').slick({
+$(document).ready(function () {
+$('.feedback-container').slick({
     dots: true,
     infinite: true,
     arrows: false,
     speed: 300,
+    autoplay:false,
+    autoplaySpeed: 3000,
     slidesToShow: 2,
     slidesToScroll: 1,
     responsive: [
         {
-            breakpoint: 1024,
+            breakpoint: 1200,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+            }
+        },
+        {
+            breakpoint: 800,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+            }
+        },
+        {
+            breakpoint: 580,
             settings: {
                 slidesToShow: 1,
-                slidesToScroll: 1,
-                infinite: true,
-                dots: true
+                slidesToScroll: 1
             }
         }
     ]
 })
 });
+
